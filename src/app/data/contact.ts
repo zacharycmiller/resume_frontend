b2d47786302a1662a14eaef2b6271e812031d342
Zachary C. Miller <nonyabusiness@empty.com>
2023-03-16 17:18:24 -0400

    user_0 Contact data moved to backend

--- src/app/data/contact.ts
+++
@@ -1,9 +0,0 @@
-import { Contact } from "../models/contact";
-
-export const CONTACT: Contact = {
-    id: 0,
-    city: "Dalzell",
-    state: "SC",
-    phoneNumber: "+1 843.499.4231", 
-    email: "mr.zcmiller@gmail.com"
-}
