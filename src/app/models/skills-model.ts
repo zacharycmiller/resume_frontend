2c02a8ebac3eb934b2d405effe8e5fd49d39f494
Zachary C. Miller <nonyabusiness@empty.com>
2023-03-17 15:28:26 -0400

    user_0 Restructured Skills and data to backend

--- src/app/models/skills-model.ts
+++ src/app/models/skills-model.ts
@@ -1,5 +1,13 @@
-export interface Skills {
+export enum SkillCategory {
+    FRONTEND = 'Front-end',
+    BACKEND = 'Back-end',
+    TESTING = 'Testing',
+    DEVOPS = 'DevOps'
+  }
+  
+  export interface Skill {
     id: number;
+    resumeId: number;
     name: string;
-    category: string;
-}
+    category: SkillCategory;
+  }
