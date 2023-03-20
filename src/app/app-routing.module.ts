ff0eda668216d82158fc2fe6a1dcde55705051d3
Zachary C. Miller <nonyabusiness@empty.com>
2023-03-20 18:38:10 -0400

    change to routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AboutMeComponent } from './components/about-me/about-me.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ResumeComponent } from './components/resume/resume.component';
 import { UserIntakeComponent } from './components/user-intake/user-intake.component';
